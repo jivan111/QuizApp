ca59c31ae5ca20c5f7949be62bdb528fcde4ffd8
jivan mainali <jivanmainali1@gmail.com>
2020-02-18 23:05:14 -0500

    dfgvfg

--- database/databaseModel.js
+++ database/databaseModel.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
-// const url="mongodb+srv://jivanbhai1:Jivan123456@cluster0-lmfsu.mongodb.net/Quizdb"
-mongoose.connect("mongodb://localhost:27017/Quizdb",{useNewUrlParser:true,useUnifiedTopology: true } )
+const url="mongodb+srv://jivanbhai1:Jivan123456@cluster0-lmfsu.mongodb.net/Quizdb"
+const localUrl="mongodb://localhost:27017/Quizdb"
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true } )
 //this is redundant as database is opened as soon as its connected
 // mongoose.connection
 //  .once("open",()=>{
